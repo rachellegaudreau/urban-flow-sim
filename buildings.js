f088fcb46ed683f48a2c2ae41bc7e490526e16f8
Rachelle Gaudreau <107381065+rachellegaudreau@users.noreply.github.com>
2025-07-09 00:24:56 -0400

    Update buildings.js

--- buildings.js
+++ buildings.js
@@ -3,3 +3,4 @@ const buildings = [
   { name: "Hospital", lat: 40.716, lng: -74.01, zone: "commercial" },
   { name: "Library", lat: 40.717, lng: -74.003, zone: "residential" },
   { name: "Market", lat: 40.713, lng: -74.007, zone: "industrial" }
+  ];
