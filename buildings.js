2639825fdca295830a45e46325e58652daca18f1
Rachelle Gaudreau <107381065+rachellegaudreau@users.noreply.github.com>
2025-07-09 00:20:13 -0400

    Create buildings.js

---
+++ buildings.js
@@ -0,0 +1,5 @@
+const buildings = [
+  { name: "School", lat: 40.719, lng: -74.005, zone: "residential" },
+  { name: "Hospital", lat: 40.716, lng: -74.01, zone: "commercial" },
+  { name: "Library", lat: 40.717, lng: -74.003, zone: "residential" },
+  { name: "Market", lat: 40.713, lng: -74.007, zone: "industrial" }
